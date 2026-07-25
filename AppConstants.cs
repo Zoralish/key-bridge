8acfb8bed045c82571308f5f9403de53b01f28b3
Andrey Tsaregorodtsev <107490456+Zoralish@users.noreply.github.com>
2026-07-25 19:21:57 -0400

    simplification

--- AppConstants.cs
+++ AppConstants.cs
@@ -4,17 +4,10 @@ public static class AppPaths
 {
     public static readonly string ConfigPath = Path.Combine(
         AppContext.BaseDirectory,
-        "config.json"
+        "keybridge-config.json"
     );
-    public static readonly string KeyStoragePath = Path.Combine(
+    public static readonly string DataProtectionKeyRingPath = Path.Combine(
         AppContext.BaseDirectory,
-        "CryptoKeys"
+        "DataProtectionKeys"
     );
-}
-
-public static class LogTags
-{
-    public static readonly string infoTag = "[cyan]" + "INFO".PadRight(7) + "[/]";
-    public static readonly string errorTag = "[red]" + "ERROR".PadRight(7) + "[/]";
-    public static readonly string successTag = "[green]" + "SUCCESS".PadRight(7) + "[/]";
 }
